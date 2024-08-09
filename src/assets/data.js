a356dd6770c5ac9647c83d1938722a4ede0d2cc3
Vikas Kanukanti <vikaskanukanti@gmail.com>
2024-08-09 22:24:51 +0530

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    question: "Which of the following is a programming language?",
+    option1: "HTML",
+    option2: "CSS",
+    option3: "JavaScript",
+    option4: "SQL",
+    ans: 3
+  },
+  {
+    question: "Which of the following is an example of an operating system?",
+    option1: "Microsoft Word",
+    option2: "Windows 10",
+    option3: "Google Chrome",
+    option4: "Photoshop",
+    ans: 2
+  },
+  {
+    question: "Which of the following is a non-volatile memory?",
+    option1: "RAM",
+    option2: "ROM",
+    option3: "Cache",
+    option4: "Register",
+    ans: 2
+  },
+  {
+    question: "Which component of a computer is responsible for executing instructions?",
+    option1: "Motherboard",
+    option2: "RAM",
+    option3: "Hard Drive",
+    option4: "CPU",
+    ans: 4
+  },
+  {
+    question: "A group of ……………… is commonly called as one byte.",
+    option1: "Six bits",
+    option2: "Eight bits",
+    option3: "Twelve bits",
+    option4: "Fifteen bits",
+    ans: 2
+  },
+  {
+    question: "Which device is required for the Internet connection?",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    ans: 1
+  },
+  {
+    question: "Which continent has the highest number of countries?",
+    option1: "Asia",
+    option2: "Europe",
+    option3: "North America",
+    option4: "Africa",
+    ans: 4
+  },
+  {
+    question: "Junk e-mail is also called?",
+    option1: "Spam",
+    option2: "Fake",
+    option3: "Archived",
+    option4: "Bin",
+    ans: 1
+  },
+  {
+    question: "A computer cannot BOOT if it does not have the?",
+    option1: "Application Software",
+    option2: "Internet",
+    option3: "Operating System",
+    option4: "Mouse",
+    ans: 3
+  },
+  {
+    question: "First page of Website is termed as?",
+    option1: "Index Page",
+    option2: "Homepage",
+    option3: "Sitemap",
+    option4: "Pen Drive",
+    ans: 2
+  }
+];
